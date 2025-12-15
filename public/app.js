4a7e2bf4338de9d34d66ff99125daf2df5129318
JhonnFer <jhoncasq23@gmail.com>
2025-12-14 20:05:38 -0500

    webcambio final

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'TU_URL_DE_SUPABASE';
-const SUPABASE_KEY = 'TU_ANON_KEY';
+const SUPABASE_URL = 'https://cufglydvzflmzmlfphwm.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1ZmdseWR2emZsbXptbGZwaHdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyOTU5ODgsImV4cCI6MjA4MDg3MTk4OH0.F8gcELQQxO6LbqxO1gqhiZwUjLT1DotLqdAmo1YvEv8';
 
 document.getElementById('resetForm').addEventListener('submit', async (e) => {
   e.preventDefault();
